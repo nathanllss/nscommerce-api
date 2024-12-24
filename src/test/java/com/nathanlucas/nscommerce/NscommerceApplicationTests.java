45eefde7451a6a79beac374ce13cf2e3bb4de5dd
Nathan <nathan.llucasl@gmail.com>
2024-12-24 08:38:58 -0300

    initializing repo

---
+++ src/test/java/com/nathanlucas/nscommerce/NscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanlucas.nscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
