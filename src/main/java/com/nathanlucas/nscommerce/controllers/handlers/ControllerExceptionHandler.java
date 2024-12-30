ef46bf5cdbf36defeb14f0fcaee2a635cd2ca33f
Nathan <nathan.llucasl@gmail.com>
2024-12-30 15:25:29 -0300

    add: custom exception on bean validation

--- src/main/java/com/nathanlucas/nscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/nathanlucas/nscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -3,9 +3,12 @@ package com.nathanlucas.nscommerce.controllers.handlers;
 import com.nathanlucas.nscommerce.Services.exceptions.DatabaseException;
 import com.nathanlucas.nscommerce.Services.exceptions.ResourceNotFoundException;
 import com.nathanlucas.nscommerce.dtos.CustomError;
+import com.nathanlucas.nscommerce.dtos.ValidationError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +27,15 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> handleDatabaseException(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(),status.value(),e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(),status.value(),"Dados inválidos",request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(),f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
