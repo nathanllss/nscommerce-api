4e74b9ffa1dd57e3b6d765234f16d3658178b2d7
Nathan <nathan.llucasl@gmail.com>
2025-01-09 08:12:29 -0300

    UserDetails and GrantedAuthority

--- src/main/java/com/nathanlucas/nscommerce/entities/User.java
+++ src/main/java/com/nathanlucas/nscommerce/entities/User.java
@@ -1,13 +1,16 @@
 package com.nathanlucas.nscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -82,6 +85,35 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
